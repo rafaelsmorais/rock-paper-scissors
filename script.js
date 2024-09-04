3b73a9fc79f2da20199e338ba75e06ff933b23b5
Rafael Morais <contato@rafaelmoraisjr.com>
2024-09-03 22:00:59 -0300

    Created functions to get the computer and human choices

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello from js!")
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let random = Math.floor(Math.random() * 3);
+
+  if (random === 0) {
+    return "Rock";
+  } else if (random === 1) {
+    return "Paper";
+  } else {
+      return "Scissors";
+  }
+}
+
+function getHumanChoice() {
+  let choice;
+  do {
+    choice = prompt("Rock, Paper or Scissors?");
+  }
+  while (choice != 'Rock' && choice != 'Paper' && choice != 'Scissors');
+  return choice;
+}
+
+console.log(getHumanChoice())
